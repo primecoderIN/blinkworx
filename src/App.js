d593eddd20784410a175cad3b45035606746d10b
Sanjeev Kumar <sanjeevk@gnrgy.com>
2022-06-21 11:58:57 +0530

    Added admin route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
 import CreateOrder from "./pages/CreateOrder";
 import OrderManagementScreen from "./pages/OrderManagementScreen";
 import AuthScreen from "./pages/AuthScreen";
+import AdminScreen from "./pages/AdminScreen";
 const App = () => {
   return (
     <>
-    <Navbar/>
-
-    <Routes>
-      <Route
-        index
-        element={
-         <AuthScreen/>
-        }
-      />
-      <Route path="/Orders" element={<OrderManagementScreen />} />
-      <Route path="/CreateOrder" element={<CreateOrder />} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route index element={<AuthScreen />} />
+        <Route path="/Orders" element={<OrderManagementScreen />} />
+        <Route path="/CreateOrder" element={<CreateOrder />} />
+        <Route path="/AdminPanel" element={<AdminScreen />} />
+      </Routes>
     </>
   );
 };
---
+++ src/pages/AdminScreen.js
@@ -0,0 +1,9 @@
+
+
+const AdminScreen = () => {
+  return (
+    <div>AdminScreen</div>
+  )
+}
+
+export default AdminScreen
